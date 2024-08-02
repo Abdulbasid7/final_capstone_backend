c26e8dadabb5be3620a59ac19420fcb6e3312909
abdulbasid7 <abdibasid92@gmail.com>
2024-08-02 17:06:44 +0300

    last commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = 'django-insecure-nbezgrjftntu#_!_-jmm0828fg3+mvjcmf78!j)fc61*tj*hu)'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-csez0bx8@9kf)r^yk=jpr7p^x1-us)z3p95(*@+@-=evim4*0$')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(",")
 
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
